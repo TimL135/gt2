bbcbf77fd494367363e3bdd07c12d7bafe32ec02
TimL135 <t.littschwager@mbd-team.de>
2023-04-05 16:11:06 +0000

    .

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div style="height: 5vh;" id="navbar ">
+    <div style="height: 5vh;" id="navbar " v-if="!gameloopInterval">
         <span @click="view = 'game'">game</span>
     </div>
     <div style="height: 95vh; width: 100vw;">
@@ -9,6 +9,7 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import Game from './views/Game.vue';
+import { gameloopInterval } from './ts/game';
 const view = ref("game")
 </script>
 <style scoped></style>
